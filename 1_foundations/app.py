b152ba0405b46eb0e5474e3492fd84e11380298a
Abhinandan7619 <abhinandandeshpande7@gmail.com>
2025-09-13 02:10:20 +0000

    My second commit

--- 1_foundations/app.py
+++ 1_foundations/app.py
@@ -6,7 +6,6 @@ import requests
 from pypdf import PdfReader
 import gradio as gr
 
-
 load_dotenv(override=True)
 
 def push(text):
@@ -16,12 +15,14 @@ def push(text):
             "token": os.getenv("PUSHOVER_TOKEN"),
             "user": os.getenv("PUSHOVER_USER"),
             "message": text,
+        print("ahahha"):
         }
     )
 
 
 def record_user_details(email, name="Name not provided", notes="not provided"):
     push(f"Recording {name} with email {email} and notes {notes}")
+    print("hello")
     return {"recorded": "ok"}
 
 def record_unknown_question(question):
